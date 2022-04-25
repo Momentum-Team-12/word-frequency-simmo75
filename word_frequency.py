13fc5ed50fbe7010dcfeaf56f20cbaa7e5bb2899
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-04-25 12:16:31 -0700

    All-but-one

--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,25 @@
+import string
+
+
 STOP_WORDS = [
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he'
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
 
 def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+
+    with open(file, 'r') as file_contents:
+        contents_string = file_contents.read()
+        contents_lower = contents_string.lower()
+        for character in string.punctuation:
+            contents_lower = contents_lower.replace(character, '')
+        contents_split = contents_lower.split()   
+        for word in contents_split:
+            if word in STOP_WORDS:
+                contents_split.remove(word)
+    print(contents_split)
 
 
 if __name__ == "__main__":
